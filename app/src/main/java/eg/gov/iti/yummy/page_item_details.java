0abe84f40a419e56d061e02d3bceb8848f6e24f3
Ahmed Abdo <Ahmed.3bdooo@gmail.com>
2023-02-04 18:20:20 +0200

    Adding the single item design

---
+++ app/src/main/java/eg/gov/iti/yummy/page_item_details.java
@@ -0,0 +1,24 @@
+package eg.gov.iti.yummy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+public class page_item_details extends AppCompatActivity {
+
+    YouTubePlayerView youTubePlayerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_item_details);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        youTubePlayerView = findViewById(R.id.videoView);
+        getLifecycle().addObserver(youTubePlayerView);
+
+    }
+}
